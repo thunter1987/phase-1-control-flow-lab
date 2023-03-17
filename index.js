a2f8ce5b4a07da26fe2d828d0708c5e0fc307d26
Tony <thunter1987@yahoo.com>
2023-03-16 22:56:11 -0500

    "Tests Passed"

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let greeting;
+  if (distance <= 400) {
+  greeting = 'This one is on me!'
+  }
+  else if (distance <= 2000) {
+    greeting = 'That will be twenty bucks.'
+  }
+  else if (distance <= 2500) {
+    greeting = 'I will gladly take your thirty bucks.'
+  }
+  else {
+    greeting = 'No can do.'
+  }
+  return greeting;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+return city ===  'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
